16f9eb3611539d66f2647fc6e47e2c5c6b5a10d5
Devu <devudevu10@gmail.com>
2024-06-08 22:30:34 +1000

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useRef } from "react";
+import { useState } from "react";
 
 function App() {
+  const showList = useRef(null);
+
+  const handleFormsubmit = (e) => {
+    e.preventDefault();
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>coming soon</p>
-      </header>
+      <div className="dispalyTyping">test</div>
+      <form action="" className="inUseForm" onSubmit={handleFormsubmit}>
+        <input type="text" placeholder="text" />
+        <button>Submit</button>
+      </form>
+      <div className="displayList" ref={showList}></div>
     </div>
   );
 }
