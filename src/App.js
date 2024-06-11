94c5101adb3625050fcf2639ac3114f992fa1cf3
DevLboffice <lb.devendra.lifebyte@gmail.com>
2024-06-11 17:40:54 +1000

    list added

--- src/App.js
+++ src/App.js
@@ -4,20 +4,24 @@ import { useRef } from "react";
 import { useState } from "react";
 
 function App() {
+  //let lists = [];
   const listRef = useRef(null);
-  const [displayList, setDisplayList] = useState("");
+  const [displayList, setDisplayList] = useState([]);
   const [displayTypedText, setDisplayTypedText] = useState("");
 
   const handleFormsubmit = (e) => {
     e.preventDefault();
-    setDisplayList(listRef.current.value);
+    //lists.push(listRef.current.value);
+   // console.log(lis)
+    setDisplayList((prevList)=>[...prevList, listRef.current.value]);
+    //listRef.current.value = "";
   };
 
   const handleTypedText = (e) => {
     setDisplayTypedText(e.target.value);
   };
   return (
-    <div className="App">
+    <div className="App d-flex flex-column justify-content-center align-items-center" style={{minHeight:"90vh"}}>
       <div className="dispaly-typing">{displayTypedText}</div>
       <form action="" className="user-form" onSubmit={handleFormsubmit}>
         <input
@@ -29,7 +33,8 @@ function App() {
         />
         <button>Submit</button>
       </form>
-      <div className="display-list">{displayList}</div>
+      <div className="display-list">{displayList.map((itm, ind)=>( <div key={ind}>{itm}</div>)
+      )}</div>
     </div>
   );
 }
