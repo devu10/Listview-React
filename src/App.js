3fe5df6bbfd1192b6a09975801ae6253843a6c90
Devu <devudevu10@gmail.com>
2024-06-11 22:07:05 +1000

    design two

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [name, setName] = useState("");
+  const handleOnChange = (e) => {
+    const { value } = e.target; //destructurng the value because value is the property of target
+    setName(value);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="wrapper"
+      style={{
+        display: "flex",
+        height: "100vh",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: "2rem",
+      }}
+    >
+      <div
+        className="userList"
+        style={{ boxShadow: "0 0 10px grey", padding: "2rem" }}
+      >
+        <div className="display">{name}</div>
+
+        <div className="form">
+          <form action="">
+            <input type="text" onChange={handleOnChange} />
+            <button>Add</button>
+          </form>
+        </div>
+        <div className="list">
+          <ul>
+            <li>one</li>
+            <li>two</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
